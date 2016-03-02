bfa14f3d0750605b3e2dd3db7ac8864b6af693a7
Fei Liu <liu.feiwood@gmail.com>
2016-03-02 10:49:58 +0100

    basic layout

--- copySemantic.sh
+++ copySemantic.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 cp semantic/dist/semantic.min.css public/stylesheets/
+mkdir -p public/stylesheets/themes
+cp -R semantic/dist/themes/* public/stylesheets/themes/
 cp semantic/dist/semantic.min.js public/javascripts
